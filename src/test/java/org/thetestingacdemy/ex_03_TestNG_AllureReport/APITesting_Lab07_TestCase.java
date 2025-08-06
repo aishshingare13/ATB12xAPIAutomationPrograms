af2e2d0decb081cc68d619939e20cf185cae24ae
Aishwarya9813 <shingareaish13@gmail.com>
2025-08-06 19:42:35 +0530

    Allure Report

---
+++ src/test/java/org/thetestingacdemy/ex_03_TestNG_AllureReport/APITesting_Lab07_TestCase.java
@@ -0,0 +1,21 @@
+package org.thetestingacdemy.ex_03_TestNG_AllureReport;
+
+import org.testng.annotations.Test;
+
+public class APITesting_Lab07_TestCase {
+
+    @Test
+    public void test_01(){
+
+    }
+
+    @Test
+    public void test_02(){
+
+    }
+
+    @Test
+    public void test_03(){
+
+    }
+}
